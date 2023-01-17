a651b996f86fb623b3696d59518cb30a88fe7207
silviadeblasi <62344404+silviadeblasi@users.noreply.github.com>
2023-01-17 20:39:18 +0100

    registrazione suoni

--- Assets/_Scripts/_Player/PlayerRaccoltaObjects.cs
+++ Assets/_Scripts/_Player/PlayerRaccoltaObjects.cs
@@ -4,8 +4,16 @@ using UnityEngine;
 
 public class PlayerRaccoltaObjects : MonoBehaviour
 {
+    private int number_object = 0;
+    private int max_object = 2;
 
-    
+    [SerializeField] public bool all_object_found = false;
+
+    private GameObject dialogueBoxClone;
+
+    [SerializeField] private GameObject object_found;
+
+
     void Start()
     {
         
@@ -17,15 +25,33 @@ public class PlayerRaccoltaObjects : MonoBehaviour
         
     }
 
-    void OnTriggerEnter(Collider collider){
+    void OnTriggerEnter(Collider collider)
+    {
 
-        if (collider.gameObject.layer == 7){
+        if (collider.gameObject.layer == 7)
+        {
 
             Debug.Log("Player has entered the trigger");
-            Destroy (collider.gameObject);
+            Destroy(collider.gameObject);
+            number_object++;
+
+            if (number_object == max_object)
+                all_object_found = true;
+
+            comunicazione_raccolta_oggetti();
 
         }
 
     }
-        
+
+    void comunicazione_raccolta_oggetti()
+    {
+        if (all_object_found == true)
+            dialogueBoxClone = (GameObject)GameObject.Instantiate(object_found, transform.position, Quaternion.identity);
+        Destroy(dialogueBoxClone, 3f);
+
+    }
+
 }
+        
+
