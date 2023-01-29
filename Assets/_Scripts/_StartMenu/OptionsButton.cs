853635bf1b88bb139923c2ea9e0a489fbd622009
Agnese <s305900@studenti.polito.it>
2023-01-29 17:52:53 +0100

    Created options menu

--- Assets/_Scripts/_StartMenu/OptionsButton.cs
+++ Assets/_Scripts/_StartMenu/OptionsButton.cs
@@ -1,18 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class OptionsButton : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Slider _volumeSlider;
+
+    [SerializeField] private AudioSource _audioSource;
+
+    public void Start()
     {
-        
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ChangeVolume()
     {
-        
+        AudioListener.volume = _volumeSlider.value;
+        Debug.Log("volume :" + _volumeSlider.value);
+    }
+
+    public void FullScreen()
+    {
+        Screen.fullScreen = !Screen.fullScreen;
+
+        Debug.Log(Screen.fullScreenMode);
+    }
+
+    public void Audio()
+    {
+        if (_audioSource.isPlaying)
+        {
+            _audioSource.Stop();
+        } else
+        {
+            _audioSource.Play();
+        }
     }
 }
+
