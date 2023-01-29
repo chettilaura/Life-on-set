7da410c54fdbc3fa8d1ea4cac46d3ddb983631f1
Agnese <s305900@studenti.polito.it>
2023-01-29 18:53:30 +0100

    cose

--- Assets/_Scripts/_Player/MouseLookAround.cs
+++ Assets/_Scripts/_Player/MouseLookAround.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class MouseLookAround : MonoBehaviour
@@ -7,25 +8,27 @@ public class MouseLookAround : MonoBehaviour
     private float _rotationX = 0f;
     private float _rotationY = 0f;
 
-    [SerializeField] private float _mouseSensitivity = 15f;
+    private float _mouseSensitivity = 7f;
 
     [SerializeField] private Transform _playerBody;
-    [SerializeField] private Vector3 _offset; 
+    [SerializeField] private Vector3 _offset;
 
 
     void Update()
     {
-
+        
         //follow player
-        transform.position = _playerBody.position + _offset;
+        Vector3 desiredPosition = _playerBody.position + _offset;
+        transform.position = desiredPosition;
+
 
         //rotation mouse
-        _rotationX += Input.GetAxis("Mouse X") * _mouseSensitivity;
+       _rotationX += Input.GetAxis("Mouse X") * _mouseSensitivity;
         _rotationY += Input.GetAxis("Mouse Y") * _mouseSensitivity;
 
-        _rotationY = Mathf.Clamp(_rotationY, -90f, 90f);
+        _rotationY = Mathf.Clamp(_rotationY, -30f, 30f);
 
-        transform.localRotation = Quaternion.Euler(-_rotationY, _rotationX, 0f);
+        transform.rotation = Quaternion.Euler(-_rotationY, _rotationX, 0f);
         
     }
 }
