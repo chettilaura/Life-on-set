1737c90f7d1e640aad8302dc7d9b3efccda5827a
Agnese <s305900@studenti.polito.it>
2023-01-30 15:37:41 +0100

    Fixed cameraFollow

--- Assets/_Scripts/_Player/MouseLookAround.cs
+++ Assets/_Scripts/_Player/MouseLookAround.cs
@@ -5,30 +5,27 @@ using UnityEngine;
 
 public class MouseLookAround : MonoBehaviour
 {
-    private float _rotationX = 0f;
+    [SerializeField] private GameObject _player;
+    public float DistanceFromPlayer = 5;
+    public float height = 2;
     private float _rotationY = 0f;
+    private float _rotationX = 0f;  
 
-    private float _mouseSensitivity = 7f;
-
-    [SerializeField] private Transform _playerBody;
-    [SerializeField] private Vector3 _offset;
-
-
-    void Update()
+    void LateUpdate()
     {
-        
-        //follow player
-        Vector3 desiredPosition = _playerBody.position + _offset;
-        transform.position = desiredPosition;
+        transform.position = _player.transform.position - _player.transform.forward * DistanceFromPlayer;
+        transform.LookAt(_player.transform.position);
+        transform.position = new Vector3(transform.position.x, transform.position.y + height, transform.position.z);
 
-
-        //rotation mouse
-       _rotationX += Input.GetAxis("Mouse X") * _mouseSensitivity;
-        _rotationY += Input.GetAxis("Mouse Y") * _mouseSensitivity;
+        //mouse rotation 
+        _rotationX += Input.GetAxis("Mouse X");
+        _rotationY += Input.GetAxis("Mouse Y");
 
         _rotationY = Mathf.Clamp(_rotationY, -30f, 30f);
+        _rotationX = Mathf.Clamp(_rotationX, -30f, 30f);
 
-        transform.rotation = Quaternion.Euler(-_rotationY, _rotationX, 0f);
-        
+        transform.Rotate(-_rotationY, _rotationX, 0f);
     }
 }
+
+
