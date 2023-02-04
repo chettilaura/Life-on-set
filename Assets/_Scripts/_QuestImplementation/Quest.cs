234970e9f26aea6b37354dff8395b953db6df703
Agnese <s305900@studenti.polito.it>
2023-02-04 20:02:42 +0100

    cose

--- Assets/_Scripts/_QuestImplementation/Quest.cs
+++ Assets/_Scripts/_QuestImplementation/Quest.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [System.Serializable]
-public class Quest 
+public class Quest : MonoBehaviour
 {
     public bool isActive;
     public string title;
     public string description;
     public bool isFinished;
+    public Goal Goal;
+
+    public void Complete()
+    {
+        isFinished = true;
+        isActive = false;   
+    }
 }
