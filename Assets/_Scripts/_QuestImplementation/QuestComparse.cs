369becc511646b33a7bae8b7d7c21003de4852a4
Agnese <s305900@studenti.polito.it>
2023-02-08 12:48:44 +0100

    sempre comparse

--- Assets/_Scripts/_QuestImplementation/QuestComparse.cs
+++ Assets/_Scripts/_QuestImplementation/QuestComparse.cs
@@ -4,20 +4,36 @@ using UnityEngine;
 
 public class QuestComparse : QuestNPC
 {
-    public Transform script;
+    public Transform comparse;
+    public Transform aliens;
+    public GameObject spawner;
 
 
     private void Update()
     {
+
         if (questNPC._inTrigger && Input.GetKeyDown(KeyCode.E))
         {
-
             QuestManager.questManager.QuestRequest(this);
-            for (int i = 0; i < script.childCount; i++)
+            if (QuestManager.questManager.currentQuest != null)
             {
-                script.GetChild(i).gameObject.SetActive(true);
+                for (int i = 0; i < comparse.childCount; i++)
+                {
+                    comparse.GetChild(i).gameObject.SetActive(true);
+                }
+                comparse.gameObject.SetActive(true);
+                for (int i = 0; i < aliens.childCount; i++)
+                {
+                    aliens.GetChild(i).gameObject.SetActive(true);
+                }
+                aliens.gameObject.SetActive(true);
+                spawner.SetActive(true);
+            } else
+            {
+                comparse.gameObject.SetActive(false);
+                aliens.gameObject.SetActive(false);
             }
-            script.gameObject.SetActive(true);
+
         }
         SetQuestMarker();
     }
