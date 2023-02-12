bbcec39e6c008a7f4076693c8cfce24d3c51e41a
silviadeblasi <62344404+silviadeblasi@users.noreply.github.com>
2023-02-12 20:22:54 +0100

    ok dialoghi con luci e differenze

--- Assets/_Scripts/_QuestImplementation/QuestDifferenze.cs
+++ Assets/_Scripts/_QuestImplementation/QuestDifferenze.cs
@@ -5,11 +5,17 @@ using UnityEngine;
 public class QuestDifferenze : QuestNPC
 {
     public GameObject startTask;
+    public GameObject dialoguebox_DOP;
+    private GameObject dialogueBoxClone;
+    public GameObject infoDOP;
+    private bool info = false;
 
     void Update()
     {
-        if (questNPC._inTrigger && Input.GetKeyDown(KeyCode.E))
+        if (questNPC._inTrigger && Input.GetKeyDown(KeyCode.E) && info == false)
         {
+            dialogueBoxClone = (GameObject)GameObject.Instantiate(infoDOP, transform.position, Quaternion.identity);
+            info = true;
             QuestManager.questManager.QuestRequest(this);
             if (QuestManager.questManager.currentQuest.id == 5)
                 startTask.GetComponent<Collider>().enabled = true;
@@ -17,5 +23,13 @@ public class QuestDifferenze : QuestNPC
                 startTask.GetComponent<Collider>().enabled = false;
         }
         SetQuestMarker();
+
+        if(info == true){
+            if ( Input.GetKeyDown(KeyCode.Space)){
+                Destroy(dialogueBoxClone);
+
+                dialogueBoxClone = (GameObject)GameObject.Instantiate(dialoguebox_DOP, transform.position, Quaternion.identity);
+            }
+        }
     }
 }
--- Assets/_Scripts/_QuestImplementation/QuestLuci.cs
+++ Assets/_Scripts/_QuestImplementation/QuestLuci.cs
@@ -8,13 +8,19 @@ public class QuestLuci : QuestNPC
     public GameObject Camera;
     public GameObject Light;
     public GameObject LightGun;
+    public GameObject dialoguebox_luci;
+    private GameObject dialogueBoxClone;
+    public GameObject infoLuci;
+    private bool info = false;
     void Update()
     {
-        if (questNPC._inTrigger && Input.GetKeyDown(KeyCode.E))
+        if (questNPC._inTrigger && Input.GetKeyDown(KeyCode.E) && info == false)
         {
             QuestManager.questManager.QuestRequest(this);
             if (QuestManager.questManager.currentQuest.id == 3)
             {
+                dialogueBoxClone = (GameObject)GameObject.Instantiate(infoLuci, transform.position, Quaternion.identity);
+                info = true;
                 //abilita il collider del relativo oggetto per iniziare la task (es. sedia regista per task differenze)
                 startTask.GetComponent<Collider>().enabled = true;
                 Light.GetComponent<ambient_light>().enabled = true;
@@ -30,7 +36,13 @@ public class QuestLuci : QuestNPC
                 Camera.SetActive(false);
             }
 
+            if(info == true){
+                if ( Input.GetKeyDown(KeyCode.Space)){
+                Destroy(dialogueBoxClone);
 
+                dialogueBoxClone = (GameObject)GameObject.Instantiate(dialoguebox_luci, transform.position, Quaternion.identity);
+            }
+        }
         }
         SetQuestMarker();
     }
