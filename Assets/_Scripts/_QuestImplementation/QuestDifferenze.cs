ba4e110b5e3f00a753c589fc036bd135e99fea2f
silviadeblasi <62344404+silviadeblasi@users.noreply.github.com>
2023-02-13 17:17:06 +0100

    studio uno

--- Assets/_Scripts/_QuestImplementation/QuestDifferenze.cs
+++ Assets/_Scripts/_QuestImplementation/QuestDifferenze.cs
@@ -8,14 +8,21 @@ public class QuestDifferenze : QuestNPC
     public GameObject dialoguebox_DOP;
     private GameObject dialogueBoxClone;
     public GameObject infoDOP;
-    private bool info = false;
+    public GameObject dialoguebox_diff_inProgress;
+    public GameObject dialoguebox_diff_completed;
+    private bool info = false; //info diventa true quando la spiegazione è stata fatta vedere  
+    private bool nonCompletedYet = false; //questa variabile diventa true quando torna dal NPC ma non ha ancora raccolto tutti i suoni 
+    private bool _coffeeReceived = false;
 
     void Update()
     {
-        if (questNPC._inTrigger && Input.GetKeyDown(KeyCode.E) && info == false)
+        if (questNPC._inTrigger && Input.GetKeyDown(KeyCode.E))
         {
+            if ( info == false ){
             dialogueBoxClone = (GameObject)GameObject.Instantiate(infoDOP, transform.position, Quaternion.identity);
             info = true;
+            }
+            
             QuestManager.questManager.QuestRequest(this);
             if (QuestManager.questManager.currentQuest.id == 5)
                 startTask.GetComponent<Collider>().enabled = true;
@@ -24,12 +31,24 @@ public class QuestDifferenze : QuestNPC
         }
         SetQuestMarker();
 
-        if(info == true){
-            if ( Input.GetKeyDown(KeyCode.Space)){
+        if(questNPC._inTrigger && info == true){
+            if (questNPC._inTrigger && Input.GetKeyDown(KeyCode.Space)){
                 Destroy(dialogueBoxClone);
 
                 dialogueBoxClone = (GameObject)GameObject.Instantiate(dialoguebox_DOP, transform.position, Quaternion.identity);
+                nonCompletedYet = true;
             }
+        }
+
+        if (questNPC._inTrigger && Input.GetKeyDown(KeyCode.E) && nonCompletedYet == true && QuestManager.questManager.currentQuest.questObjectiveCount != 3){
+            //esce dialogo "non hai ancora completato il task"
+            dialogueBoxClone = (GameObject)GameObject.Instantiate(dialoguebox_diff_inProgress, transform.position, Quaternion.identity);
+        }
+
+        if (questNPC._inTrigger && Input.GetKeyDown(KeyCode.E) && QuestManager.questManager.currentQuest.questObjectiveCount == 3 ){
+            //esce dialogo " hai completato il task" & duiventa verde 
+            dialogueBoxClone = (GameObject)GameObject.Instantiate(dialoguebox_diff_completed, transform.position, Quaternion.identity);
+
         }
     }
 }
--- Assets/_Scripts/_QuestImplementation/QuestLuci.cs
+++ Assets/_Scripts/_QuestImplementation/QuestLuci.cs
@@ -11,16 +11,24 @@ public class QuestLuci : QuestNPC
     public GameObject dialoguebox_luci;
     private GameObject dialogueBoxClone;
     public GameObject infoLuci;
-    private bool info = false;
+    public GameObject dialoguebox_luci_completed;
+    public GameObject dialoguebox_luci_inProgress;
+    private bool info = false; //info diventa true quando la spiegazione è stata fatta vedere  
+    private bool nonCompletedYet = false; //questa variabile diventa true quando torna dal NPC ma non ha ancora raccolto tutti i suoni 
+    private bool _coffeeReceived = false;
+ 
     void Update()
     {
-        if (questNPC._inTrigger && Input.GetKeyDown(KeyCode.E) && info == false)
+        if (questNPC._inTrigger && Input.GetKeyDown(KeyCode.E))
         {
+            if(info == false){
+                dialogueBoxClone = (GameObject)GameObject.Instantiate(infoLuci, transform.position, Quaternion.identity);
+                info = true;
+            }
             QuestManager.questManager.QuestRequest(this);
             if (QuestManager.questManager.currentQuest.id == 3)
             {
-                dialogueBoxClone = (GameObject)GameObject.Instantiate(infoLuci, transform.position, Quaternion.identity);
-                info = true;
+               
                 //abilita il collider del relativo oggetto per iniziare la task (es. sedia regista per task differenze)
                 startTask.GetComponent<Collider>().enabled = true;
                 Light.GetComponent<ambient_light>().enabled = true;
@@ -38,11 +46,24 @@ public class QuestLuci : QuestNPC
         }
         SetQuestMarker();
 
-        if(info == true){
-            if (Input.GetKeyDown(KeyCode.Space)){
+        if(questNPC._inTrigger && info == true){
+            if (questNPC._inTrigger && Input.GetKeyDown(KeyCode.Space)){
                 Destroy(dialogueBoxClone);
+
                 dialogueBoxClone = (GameObject)GameObject.Instantiate(dialoguebox_luci, transform.position, Quaternion.identity);
+                nonCompletedYet = true;
             }
-         }
+        }
+
+        if (questNPC._inTrigger && Input.GetKeyDown(KeyCode.E) && nonCompletedYet == true && QuestManager.questManager.currentQuest.questObjectiveCount != 3){
+            //esce dialogo "non hai ancora completato il task"
+            dialogueBoxClone = (GameObject)GameObject.Instantiate(dialoguebox_luci_inProgress, transform.position, Quaternion.identity);
+        }
+
+        if (questNPC._inTrigger && Input.GetKeyDown(KeyCode.E) && QuestManager.questManager.currentQuest.questObjectiveCount == 3 ){
+            //esce dialogo " hai completato il task" & duiventa verde 
+            dialogueBoxClone = (GameObject)GameObject.Instantiate(dialoguebox_luci_completed, transform.position, Quaternion.identity);
+
+        }
     }
 }
