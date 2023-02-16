124b2c2c799bbb3ab43f5a607a893e79e254c61a
chettilaura <98lauramarchetti@gmail.com>
2023-02-16 14:24:39 +0100

    start manu con camandi

--- Assets/_Scripts/tutorial_related/player_tutorial_appears.cs
+++ Assets/_Scripts/tutorial_related/player_tutorial_appears.cs
@@ -4,7 +4,19 @@ using UnityEngine;
 
 public class player_tutorial_appears : MonoBehaviour
 {
-    public GameObject tutorial_prefab;
+
+    public GameObject tutorial_canvas_prefab;
+    public tutorial_una_sola_volta tutorial_una_sola_volta;
+
+    void Update(){
+    if(tutorial_una_sola_volta.tutorial_attivo == true){
+        tutorial_canvas_prefab.SetActive(true);
+    }else{
+        tutorial_canvas_prefab.SetActive(false);
+    }
+
+}
+    /*public GameObject tutorial_prefab;
     void Update(){
      if (Input.GetKeyDown(KeyCode.Question) )
         {
@@ -18,5 +30,9 @@ public class player_tutorial_appears : MonoBehaviour
                 tutorial_prefab.SetActive(false);
             }
         }
-    }
+    }*/
+
+
+
+
 }
---
+++ Assets/_Scripts/tutorial_related/tutorial_una_sola_volta.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tutorial_una_sola_volta : MonoBehaviour
+{
+    public bool tutorial_attivo = true;
+
+    public void disattiva_tutorial(){
+        tutorial_attivo = false;
+    }
+
+
+
+
+}
