d42f398e280ec2ae3de5a4ce71ac1a085afc34f6
Agnese <s305900@studenti.polito.it>
2023-02-16 20:08:13 +0100

    voglio uccidermi

--- Assets/_Scripts/NPCs_related/extrasMovement.cs
+++ Assets/_Scripts/NPCs_related/extrasMovement.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Net.NetworkInformation;
+//using System.Net.NetworkInformation;
 using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
